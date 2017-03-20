3c4de8739307ba9978d5885c83bf987a255fc947
David Marland <djmarland@users.noreply.github.com>
2017-03-20 10:13:43 +0000

    First pass at using Gulp for asset compilation

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 ===========
 
 /programmes version 3. Finally.
+
+### Static Assets
+Using Yarn allows the dependencies to be properly locked, and can abstract away
+the need to install other packages globally.
+[Install Yarn](https://yarnpkg.com/en/docs/install) first.
+
+Then perform `yarn install`
+
+To perform the development tasks
+`yarn run dev`
+
+To perform the dist tasks (for ci)
+`yarn run dist`
+
+To watch for file changes
+`yarn run watch`
---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const gulp = require('gulp');
+const args = require('yargs').argv;
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+
+const staticPathSrc = 'src/AppBundle/Resources';
+const staticPathDist = 'web/assets';
+const sassMatch = '/sass/**/*.scss';
+
+const env = args.env || 'dev';
+
+gulp.task('images', function() {
+    gulp.src(staticPathSrc + '/images/**/*.*')
+        .pipe(gulp.dest(staticPathDist + '/images/'));
+});
+
+gulp.task('sass', function() {
+    if (env === 'dev') {
+        return gulp.src(staticPathSrc + sassMatch)
+            .pipe(sourcemaps.init())
+            .pipe(sass().on('error', sass.logError))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest(staticPathDist + '/css/'));
+    }
+
+    return gulp.src(staticPathSrc + sassMatch)
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest(staticPathDist + '/css/'));
+});
+
+gulp.task('watch',function() {
+    gulp.watch(staticPathSrc + sassMatch,['sass']);
+});
+
+gulp.task('default', ['sass', 'images']);
