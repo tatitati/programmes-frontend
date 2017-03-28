dbbbe389a38f38e710466cdfe6333e61f9fbe290
Ben Scott <ben.scott@bbc.co.uk>
2017-03-28 16:55:27 +0100

    Add test for StatusController

---
+++ tests/Controller/StatusControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use Tests\AppBundle\BaseWebTestCase;
+
+class StatusControllerTest extends BaseWebTestCase
+{
+    public function testStatus()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/status');
+        $this->assertResponseStatusCode($client, 200);
+
+        $this->assertEquals('YES', $crawler->filter('[data-test-name=db-connectivity] span')->text());
+    }
+
+    public function testStatusFromElb()
+    {
+        $client = static::createClient([], [
+            'HTTP_USER_AGENT' => 'ELB-HealthChecker/1.0',
+        ]);
+        $crawler = $client->request('GET', '/status');
+
+        $this->assertResponseStatusCode($client, 200);
+        $this->assertEquals('OK', $client->getResponse()->getContent());
+    }
+}
