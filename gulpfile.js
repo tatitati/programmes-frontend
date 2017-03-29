2ba36cd5cf4cf7af022682e3fc3a94d51d8150e0
Francisco Albert <francis.jaa@gmail.com>
2017-03-29 14:57:44 +0100

    [ Build ] - Static assets
    Added static assets build with gulp-rev and configuration for symfony updated to run from manifest in prod

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,54 @@
 'use strict';
 
 const gulp = require('gulp');
-const args = require('yargs').argv;
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
+const rev = require('gulp-rev');
+const revdelOriginal = require('gulp-rev-delete-original');
+const del = require('del');
 
 const staticPathSrc = 'src/Resources';
 const staticPathDist = 'web/assets';
 const sassMatch = '/sass/**/*.scss';
+const imageMatch = '/images/*';
 
-const env = args.env || 'dev';
-
-gulp.task('images', function() {
-    gulp.src(staticPathSrc + '/images/**/*.*')
-        .pipe(gulp.dest(staticPathDist + '/images/'));
+gulp.task('css:clean', function() {
+    return del([staticPathDist + '/css']);
 });
 
-gulp.task('sass', function() {
-    if (env === 'dev') {
-        return gulp.src(staticPathSrc + sassMatch)
-            .pipe(sourcemaps.init())
-            .pipe(sass().on('error', sass.logError))
-            .pipe(sourcemaps.write())
-            .pipe(gulp.dest(staticPathDist + '/css/'));
-    }
+gulp.task('images:clean', function() {
+    return del([staticPathDist + '/images']);
+});
 
+gulp.task('sass', ['css:clean'], function() {
     return gulp.src(staticPathSrc + sassMatch)
+        .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest(staticPathDist + '/css/'));
 });
 
+gulp.task('images', ['images:clean'], function() {
+    return gulp.src(staticPathSrc + '/images/**/*')
+        .pipe(gulp.dest(staticPathDist + '/images/'));
+});
+
+gulp.task('rev', ['sass', 'images'], function() {
+    return gulp.src([staticPathDist + '/**/*', '!' + staticPathDist + '/**/rev-manifest.json'])
+        .pipe(rev())
+        .pipe(gulp.dest(staticPathDist))
+        .pipe(revdelOriginal()) // delete no-revised file
+        .pipe(rev.manifest('rev-manifest.json'))
+        .pipe(gulp.dest(staticPathDist));
+});
+
+/*
+ * Entry tasks
+ */
 gulp.task('watch',function() {
     gulp.watch(staticPathSrc + sassMatch,['sass']);
+    gulp.watch(staticPathSrc + imageMatch,['images']);
 });
 
 gulp.task('default', ['sass', 'images']);
+gulp.task('distribution', ['rev']);
