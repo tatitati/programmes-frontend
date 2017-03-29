b0089d417a5c4f3c100224e6468597ffd04f7383
Ben Scott <ben.scott@bbc.co.uk>
2017-03-29 14:23:44 +0100

    Use controller service arguments in StatusController too

--- src/Controller/StatusController.php
+++ src/Controller/StatusController.php
@@ -6,19 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use DateTimeImmutable;
+use Doctrine\DBAL\Connection;
 
 class StatusController extends Controller
 {
-    public function __invoke(Request $request): Response
+    public function statusAction(Request $request, Connection $dbalConnection): Response
     {
+        // TODO: Name this method __invoke rather than statusAction if
+        // "controller.service_arguments" becomes supported on invokable controllers
+        // https://github.com/symfony/symfony/issues/22202
+
         // If the load balancer is pinging us then give them a plain OK
         if ($request->headers->get('User-Agent') == 'ELB-HealthChecker/1.0') {
             return new Response('OK', Response::HTTP_OK, ['content-type' => 'text/plain']);
         }
 
         // Other people get a better info screen
-        $dbalConnection = $this->get('doctrine.dbal.default_connection');
-
         return $this->render('@App/status/status.html.twig', [
             'now' => new DateTimeImmutable(),
             'dbConnectivity' => $dbalConnection->isConnected() || $dbalConnection->connect(),
