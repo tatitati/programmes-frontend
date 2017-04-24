51df4ceec7c6507f9603a2e6d3032f8af48a64e0
Ben Scott <ben.scott@bbc.co.uk>
2017-04-24 16:01:01 +0100

    Use a raw query to get the programmes count.
    This is naughty, but we'll make an exception. Working out the accurate
    count by excluding embargos and filtering by type takes too long, so use
    a raw query to find the table size.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,15 +4,20 @@ namespace App\Controller;
 
 use BBC\ProgrammesPagesService\Service\NetworksService;
 use BBC\ProgrammesPagesService\Service\ProgrammesService;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\ResultSetMapping;
 
 class HomeController extends BaseController
 {
-    public function __invoke(ProgrammesService $programmesService, NetworksService $networksService)
+    public function __invoke(EntityManagerInterface $em, NetworksService $networksService)
     {
-        $programmeCount = $programmesService->countAll();
+        $programmeCount = $this->countProgrammesAndGroups($em);
 
         $serviceTypes = ['TV', 'National Radio', 'Regional Radio', 'Local Radio'];
-        $networks = $networksService->findPublishedNetworksByType($serviceTypes, NetworksService::NO_LIMIT);
+        $networks = $networksService->findPublishedNetworksByType(
+            $serviceTypes,
+            NetworksService::NO_LIMIT
+        );
 
         return $this->renderWithChrome('home/show.html.twig', [
             'programmeCount' => $programmeCount,
@@ -27,8 +32,24 @@ class HomeController extends BaseController
     {
         // We don't need to sort after filtering as the array of
         // networks was already in order
-        return  array_filter($networks, function ($network) use ($type) {
+        return array_filter($networks, function ($network) use ($type) {
             return $network->getType() == $type;
         });
+    }
+
+    private function countProgrammesAndGroups(EntityManagerInterface $em)
+    {
+        // This is very naughty, as all Programmes query logic should be in
+        // programmes-pages-service. But we'll make an exception here as it
+        // really is a one-off.
+        // Use a native query here because providing an accurate value by
+        // excluding embargoed items takes too long, and it's not worth adding
+        // an index to the table for this one query.
+        $qText = "SELECT COUNT(*) as cnt FROM core_entity c";
+
+        $rms = new ResultSetMapping();
+        $rms->addScalarResult('cnt', 'cnt');
+
+        return $em->createNativeQuery($qText, $rms)->getSingleScalarResult();
     }
 }
