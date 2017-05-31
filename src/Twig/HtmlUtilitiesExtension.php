f3efca05fb328d678b6eab1b928f9665a82fd736
Ben Scott <ben.scott@bbc.co.uk>
2017-05-31 18:57:18 +0100

    Move asset_js into HtmlUtilitiesExtension and add test
    Avoiding a proliferation of Twig Extensions feels like a sensible idea

--- src/Twig/HtmlUtilitiesExtension.php
+++ src/Twig/HtmlUtilitiesExtension.php
@@ -2,19 +2,37 @@
 declare(strict_types = 1);
 namespace App\Twig;
 
+use Symfony\Component\Asset\Packages;
 use Twig_Extension;
 use Twig_Function;
 
 class HtmlUtilitiesExtension extends Twig_Extension
 {
+    private $packages;
+
+    public function __construct(Packages $packages)
+    {
+        $this->packages = $packages;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function getFunctions()
     {
         return [
+            new Twig_Function('asset_js', [$this, 'assetJs']),
             new Twig_Function('build_css_classes', [$this, 'buildCssClasses']),
         ];
+    }
+
+    /**
+     * Given a js asset path, return it in a format suitable for inclusion in
+     * the require config (i.e. with the ".js" extension removed)
+     */
+    public function assetJs(string $path): string
+    {
+        return preg_replace('/\.js$/', '', $this->packages->getUrl($path, null));
     }
 
     public function buildCssClasses(array $cssClassTests = []): string
--- tests/Twig/HtmlUtilitiesExtensionTest.php
+++ tests/Twig/HtmlUtilitiesExtensionTest.php
@@ -4,12 +4,27 @@ namespace Tests\App\Twig;
 
 use App\Twig\HtmlUtilitiesExtension;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Asset\Packages;
 
 class HtmlUtilitiesExtensionTest extends TestCase
 {
+    public function testAssetJs()
+    {
+        $mockPackages = $this->createMock(Packages::class);
+        $mockPackages->expects($this->once())->method('getUrl')
+            ->with('some/example/path.js')
+            ->willReturn('some/example/path-123');
+
+        $extension = new HtmlUtilitiesExtension($mockPackages);
+        $this->assertSame('some/example/path-123', $extension->assetJs('some/example/path.js'));
+    }
+
     public function testBuildCssClasses()
     {
-        $extension = new HtmlUtilitiesExtension();
+        $extension = new HtmlUtilitiesExtension(
+            $this->createMock(Packages::class)
+        );
+
         $this->assertSame('foo baz qux', $extension->buildCssClasses([
             'foo' => true,
             'bar' => false,
