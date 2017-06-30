b20cedc997e1654c16041bbdd243df2edff80a13
Francisco Albert <francis.jaa@gmail.com>
2017-06-30 13:28:51 +0100

    Fix lazy load (#75)
    * Refactoring image lazy load

--- app/Resources/js/images.js
+++ app/Resources/js/images.js
@@ -1,91 +1,46 @@
-/*
-This module lazy load images and convert them in case that they are <div> elements
-to the proper image. This lazy-loading is triggered by the scroll.
- */
 define(['jquery', 'jquery.appear'], function ($) {
-
     var Images = function (options) {
-        this.setOptions(options);
+        this.options = $.extend({}, this.options, options);
     };
 
     Images.prototype = {
         options : {
             context : $('body'),
-            data_img_src : 'data-img-src-',
-            data_srcset: 'data-srcset',
-            data_blank_img : 'data-img-blank',
             lazy_img_css : 'image--lazy',
-            valid_img_attributes : ['class', 'id', 'title', 'name'],
+            valid_divdata_attributes: ["data-src", "data-srcset", "data-sizes", "data-alt", "data-img-blank"],
             appear : {
                 onscroll : true,
                 Y: 200
             }
         },
-        setOptions : function (options) {
-            this.options = $.extend({}, this.options, options);
-        },
-        init : function () {
-            var context = (context) ? context : this.options.context;
-
+        init: function () {
             if (this.options.appear.onscroll) {
-                this.addOnScrollHandler(context);
+                this.addOnScrollHandler(this.options.context);
             }
         },
-        hasImgTag : function (element) {
+        hasImgTag: function (element) {
             return (element.tagName.toLowerCase() === 'img');
         },
-
-        convertDivToImage : function (div) {
+        convertDivToImage: function (div) {
             var divAttributes = (div.attributes) ? div.attributes : $(div)[0].attributes;
 
             var divAttribute;
-            var img = document.createElement("img");
+            var newImg = document.createElement("img");
 
             for (var i = 0; (divAttribute = divAttributes[i]) != null; i++) {
-                attrName = divAttribute.name;
-                attrValue = divAttribute.value;
+                var attrName = divAttribute.name;
+                var attrValue = divAttribute.value;
 
-                if (this.isValidImgAttribute(divAttribute.name) && divAttribute.value) {
-                    img.setAttribute(attrName, attrValue);
-                } else {
-                    switch (attrName) {
-                        case 'data-srcset':
-                            img.srcset = attrValue;
-                            break;
-                        case 'data-sizes':
-                            img.sizes = attrValue;
-                            break;
-                        case 'data-alt':
-                            img.alt = attrValue;
-                            break;
-                        default:
-                            break;
-                    }
+                if ($.inArray(attrName, this.options.valid_divdata_attributes) != -1) {
+                    attrName = attrName.replace("data-", "")
+
                 }
+
+                newImg.setAttribute(attrName, attrValue);
             }
 
-            img.src = this.getImgAttrFromDiv(div);
-
-            return img;
+            return newImg;
         },
-
-        isValidImgAttribute : function (attr) {
-            return ($.inArray(attr, this.options.valid_img_attributes) !== -1);
-        },
-
-        getImgAttrFromDiv: function(div){
-            // by default we set the src attr to the first srcset url.
-            var srcsets = div.getAttribute(this.options.data_srcset);
-
-            var url;
-            if (srcsets) {
-                var parts = srcsets[0].split(' ');
-                url = parts[0];
-            }
-
-            return (url ? url : div.getAttribute(this.options.data_blank_img));
-        },
-
         switchDiv2Img: function(div) {
             var img = this.convertDivToImage(div);
 
@@ -97,41 +52,42 @@ define(['jquery', 'jquery.appear'], function ($) {
 
             return img;
         },
-        addOnScrollHandler : function (context) {
-            var _this = this;
+        addOnScrollHandler : function () {
+            var lazyImages = $('.' + this.options.lazy_img_css, this.options.context);
+
+            var self = this;
 
             // if current image is in viewport, show it
-            $('.' + this.options.lazy_img_css, context).appear(function() {
-                _this.displayImageOnViewport(this);
+            lazyImages.appear(function() {
+                var imageAppearing = this;
+                self.displayImageOnViewport(imageAppearing);
             });
 
             // if the image is close the viewport, display it
             $(window).on('scroll', function() {
-                _this.anticipateToViewport();
+                self.anticipateToViewport();
             });
         },
         displayImageOnViewport : function (img) {
-            var _this = this;
             if (!this.hasImgTag(img)) {
-                 img = _this.switchDiv2Img(img);
+                 img = this.switchDiv2Img(img);
             }
 
-            // display converted image
-            _this.showImage(img);
+            this.showImage(img);
         },
         showImage: function(img) {
-            var _this = this;
+            var self = this;
             $(img).bind('load', function() {
-                $(img).removeClass(_this.options.lazy_img_css).unbind('load');
+                $(img).removeClass(self.options.lazy_img_css);
             });
         },
-        anticipateToViewport: function(context) {
-            var _this = this;
-            var hiddenImages = $('.' + this.options.lazy_img_css, context);
+        anticipateToViewport: function() {
+            var hiddenImages = $('.' + this.options.lazy_img_css, this.options.context);
 
+            var self = this;
             hiddenImages.each(function(index, element){
-                if (_this.isElementNearViewport(element)) {
-                    _this.displayImageOnViewport(element);
+                if (self.isElementNearViewport(element)) {
+                    self.displayImageOnViewport(element);
                 }
             });
         },
@@ -150,5 +106,6 @@ define(['jquery', 'jquery.appear'], function ($) {
         }
 
     };
+
     return Images;
 });
