2af75412663d8a5accce1627633d4885fe3e9925
Ben Scott <ben.scott@bbc.co.uk>
2017-11-03 12:55:01 +0000

    Add back the popup player link for west dorset
    This is a temporary fix and once we start linking to playspace URLs
    then this annoyance shall go away. At that time we should also remove
    reference to popup.js.
    
    Fixes PROGRAMMES-6008

--- src/DsShared/Helpers/LiveBroadcastHelper.php
+++ src/DsShared/Helpers/LiveBroadcastHelper.php
@@ -136,7 +136,7 @@ class LiveBroadcastHelper
 
         'bbc_indonesian_radio'                  => ['worldservice_liveradio', ['language' => 'indonesia', 'sid' => 'bbc_indonesian_radio']],
 
-        'bbc_radio_solent_west_dorset'          => ['network', ['networkUrlKey' => 'radiosolent']],
+        'bbc_radio_solent_west_dorset'          => ['popup_player', ['sid' => 'bbc_radio_solent_west_dorset']],
         'bbc_radio_scotland_mw'                 => ['network', ['networkUrlKey' => 'radioscotland']],
         'bbc_world_service_americas'            => ['network', ['networkUrlKey' => 'worldserviceradio']],
         'bbc_world_service_australasia'         => ['network', ['networkUrlKey' => 'worldserviceradio']],
