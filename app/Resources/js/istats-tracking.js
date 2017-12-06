d30288e095dc55180a9770435e310f9ffe156976
tasio <tasiobg@gmail.com>
2017-12-06 11:24:44 +0000

    PROGRAMMES-5956 add tracking to social bar in brand page MAP (#328)
    - created a function to enable tracking custom link_location for external link
    - add tracking to social bar
    - convert quotes in istats-tracking.js to single quotes, looks more consistent

--- app/Resources/js/istats-tracking.js
+++ app/Resources/js/istats-tracking.js
@@ -15,6 +15,7 @@ define(['jquery-1.9', 'istats-1'], function ($, istats) {
         },
         init: function () {
             this.trackLinks();
+            this.trackCustomExternalLinks();
             this.hardcodedItems();
         },
         trackLinks: function (context) {
@@ -24,7 +25,21 @@ define(['jquery-1.9', 'istats-1'], function ($, istats) {
             var links = context.find('[' + this.options.trackingAttribute + ']');
             links.each(function(){
                 label = $(this).attr(_this.options.trackingAttribute);
-                istats.track("internal", {
+                istats.track('internal', {
+                    region : $(this),
+                    linkLocation : _this.options.labelPrefix + label
+                });
+            });
+        },
+        trackCustomExternalLinks: function() {
+            // External link tracking is done 'out of the box' by iStats, this functions allows to add a custom
+            // link_location for the existing tracking
+            var _this = this,
+                label;
+            var links = $('body').find('[data-extlinktrack]');
+            links.each(function(){
+                label = $(this).attr('data-extlinktrack');
+                istats.track('external', {
                     region : $(this),
                     linkLocation : _this.options.labelPrefix + label
                 });
@@ -33,8 +48,8 @@ define(['jquery-1.9', 'istats-1'], function ($, istats) {
         hardcodedItems: function () {
             // Because of the nature of these items we can't add the "data-linktrack" attribute inside the HTML so
             // it is required to hardcode a list of custom "istats.track" calls
-            istats.track("internal", {
-                region : $(".br-masthead .service-brand-logo-master"),
+            istats.track('internal', {
+                region : $('.br-masthead .service-brand-logo-master'),
                 linkLocation : 'programmes_global_ribbon'
             });
         }
