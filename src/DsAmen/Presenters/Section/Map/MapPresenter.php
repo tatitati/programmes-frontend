aebf246eb15cdaae3b3598b667034aca76496a67
Michael Gwynne <votemike@users.noreply.github.com>
2017-12-20 12:07:33 +0000

    PROGRAMMES-5930 Adding minimap logic to last on column (#346)

--- src/DsAmen/Presenters/Section/Map/MapPresenter.php
+++ src/DsAmen/Presenters/Section/Map/MapPresenter.php
@@ -170,7 +170,7 @@ class MapPresenter extends Presenter
 
         // Add columns to map
         if ($this->isWorldNews()) {
-            $this->rightColumns[] = new LastOnPresenter($this->programme, $this->lastOn);
+            $this->rightColumns[] = new LastOnPresenter($this->programme, $this->lastOn, ['show_mini_map' => $this->showMiniMap]);
         } else {
             $hasAnUpcomingEpisode = $this->upcomingBroadcast && $this->upcomingBroadcast->getProgrammeItem()->getStreamableFrom() && !$this->upcomingBroadcast->getProgrammeItem()->isStreamable();
 
