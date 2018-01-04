617edae46e2b8d5598b61276c82668819e7f3753
Ben Scott <ben.scott@bbc.co.uk>
2018-01-04 14:08:06 -0800

    Custom conifg loading for derivative envs
    Keeping the standard config loading mechanism results in a lot of
    boilerplate and potential for doing things out of order.
    
    Introduce the concept of derivative environments in the Kernel's config
    loader so that prod_int automatically loads in prod's config beforehand
    and prod_int_fixture automatically loads in prod's config, prod_int's
    config and the fixture config.
    
    This also fixes the config load ordering for these derived environments
    so the load ordering between the packages folder and services_*.yaml
    works in the way you'd expect.

--- src/Kernel.php
+++ src/Kernel.php
@@ -47,6 +47,26 @@ class Kernel extends BaseKernel
 
     protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader)
     {
+        // Some environments inherit the configuration of others. For instance
+        // `prod_int` should load all the config for the `prod` environment
+        // before loading the config for `prod_int`. The key is an environment,
+        //and the value is an array of configs that should be loaded prior to
+        // the current environment's config.
+        // e.g. If the env is `prod_int_fixture` then the following config paths
+        // shall be loaded in the order: `prod`, `prod_int`, `prod_int_fixture`
+        $derivativeEnvs = [
+            'prod_int' => ['prod'],
+            'prod_test' => ['prod'],
+            'dev_fixture' => ['dev'],
+            'prod_int_fixture' => ['prod', 'prod_int'],
+            'prod_test_fixture' => ['prod', 'prod_test'],
+        ];
+
+        $envsList = [$this->environment];
+        if (array_key_exists($this->environment, $derivativeEnvs)) {
+            array_unshift($envsList, ...$derivativeEnvs[$this->environment]);
+        }
+
         $container->setParameter('container.autowiring.strict_mode', true);
         $container->setParameter('container.dumper.inline_class_loader', true);
         $confDir = $this->getProjectDir() . '/config';
@@ -56,11 +76,22 @@ class Kernel extends BaseKernel
         $loader->load($confDir . '/parameters.yaml', 'glob');
 
         $loader->load($confDir . '/packages/*' . self::CONFIG_EXTS, 'glob');
-        if (is_dir($confDir . '/packages/' . $this->environment)) {
-            $loader->load($confDir . '/packages/' . $this->environment . '/**/*' . self::CONFIG_EXTS, 'glob');
+
+        foreach ($envsList as $envName) {
+            if (is_dir($confDir . '/packages/' . $envName)) {
+                $loader->load($confDir . '/packages/' . $envName . '/**/*' . self::CONFIG_EXTS, 'glob');
+            }
         }
+
+        // If we are in a fixtured environment then load the fixture config
+        if (substr($this->environment, -8) == '_fixture') {
+            $loader->load($confDir . '/fixture_db.yaml', 'glob');
+        }
+
         $loader->load($confDir . '/services' . self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir . '/services_' . $this->environment . self::CONFIG_EXTS, 'glob');
+        foreach ($envsList as $envName) {
+            $loader->load($confDir . '/services_' . $envName . self::CONFIG_EXTS, 'glob');
+        }
     }
 
     protected function configureRoutes(RouteCollectionBuilder $routes)
