ecf572d16187ef9097d7a625f0f7293e6cdad3b5
Ben Scott <ben@reload.me.uk>
2018-02-02 08:55:01 -0800

    PROGRAMMES-6162 - Episode Guide Outline (#403)

--- src/Controller/ProgrammeEpisodes/GuideController.php
+++ src/Controller/ProgrammeEpisodes/GuideController.php
@@ -1,16 +1,50 @@
 <?php
-declare(strict_types=1);
-
+declare(strict_types = 1);
 namespace App\Controller\ProgrammeEpisodes;
 
 use App\Controller\BaseController;
+use App\Ds2013\Presenters\Utilities\Paginator\PaginatorPresenter;
 use BBC\ProgrammesPagesService\Domain\Entity\ProgrammeContainer;
+use BBC\ProgrammesPagesService\Service\ProgrammesService;
 
 class GuideController extends BaseController
 {
-    public function __invoke(ProgrammeContainer $programmeContainer)
-    {
-        $this->setContextAndPreloadBranding($programmeContainer);
-        return $this->renderWithChrome('programme_episodes/guide.html.twig', []);
+    public function __invoke(
+        ProgrammeContainer $programme,
+        string $extension,
+        ProgrammesService $programmesService
+    ) {
+        $this->setContextAndPreloadBranding($programme);
+        $page = $this->getPage();
+        $limit = 30;
+
+        $children = $programmesService->findEpisodeGuideChildren(
+            $programme,
+            $limit,
+            $page
+        );
+
+        // If you visit an out-of-bounds page then throw a 404. Page one should
+        // always be a 200 so search engines don't drop their reference to the
+        // page if a programme has no episodes
+        if (!$children && $page !== 1) {
+            throw $this->createNotFoundException('Page does not exist');
+        }
+
+        $paginator = null;
+
+        if ($children) {
+            $totalChildrenCount = $programmesService->countEpisodeGuideChildren($programme);
+
+            if ($totalChildrenCount > $limit) {
+                $paginator = new PaginatorPresenter($page, $limit, $totalChildrenCount);
+            }
+        }
+
+        return $this->renderWithChrome('programme_episodes/guide' . $extension . '.html.twig', [
+            'programme' => $programme,
+            'episodes' => $children,
+            'paginatorPresenter' => $paginator,
+        ]);
     }
 }
