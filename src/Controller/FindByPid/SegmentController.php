67f00d2036c36556aa23625d812ed8d693eb4288
Ben Scott <ben@reload.me.uk>
2018-04-19 02:46:32 -0700

    First pass at adding Segment find-by-pid pages (#503)
    * First pass at adding Segment find-by-pid pages
    
    Adds the details section at the top of the page, and requests all data
    required in the Controller.
    
    We still need to format the output of programmes the Segement appeared
    on, and work out how to deal with the case when a Segment appears on
    multiple programmes within the same Network.
    
    * Add Programmes that the segment appears on
    
    * FIND-BY More

--- src/Controller/FindByPid/SegmentController.php
+++ src/Controller/FindByPid/SegmentController.php
@@ -3,7 +3,13 @@ declare(strict_types = 1);
 namespace App\Controller\FindByPid;
 
 use App\Controller\BaseController;
+use BBC\ProgrammesPagesService\Domain\Entity\CoreEntity;
+use BBC\ProgrammesPagesService\Domain\Entity\Network;
+use BBC\ProgrammesPagesService\Domain\Entity\Programme;
 use BBC\ProgrammesPagesService\Domain\Entity\Segment;
+use BBC\ProgrammesPagesService\Service\CoreEntitiesService;
+use BBC\ProgrammesPagesService\Service\SegmentEventsService;
+use App\DsShared\Helpers\PlayTranslationsHelper;
 
 /**
  * Segment Page
@@ -12,12 +18,146 @@ use BBC\ProgrammesPagesService\Domain\Entity\Segment;
  */
 class SegmentController extends BaseController
 {
-    public function __invoke(Segment $segment)
-    {
-        $this->setContextAndPreloadBranding($segment);
+    public function __invoke(
+        Segment $segment,
+        PlayTranslationsHelper $playTranslationsHelper,
+        CoreEntitiesService $coreEntitiesService,
+        SegmentEventsService $segmentEventsService
+    ) {
+        $segmentEvents = $segmentEventsService->findBySegmentFull($segment, true, $segmentEventsService::NO_LIMIT);
 
-        return $this->renderWithChrome('find_by_pid/example_entity.html.twig', [
+        // findBySegmentFull returns the oldest SegmentEvent first, but we want
+        // to display the most recent first
+        $programmes = array_reverse(array_map(function ($se) {
+            return $se->getVersion()->getProgrammeItem();
+        }, $segmentEvents));
+
+        $segmentOwner = $this->getSegmentOwner($programmes, $coreEntitiesService);
+
+        $this->setIstatsProgsPageType('programmes_segment');
+        $this->setContextAndPreloadBranding($segmentOwner);
+        if ($segmentOwner) {
+            $this->setInternationalStatusAndTimezoneFromContext($segmentOwner);
+        }
+
+        return $this->renderWithChrome('find_by_pid/segment.html.twig', [
             'segment' => $segment,
+            'segmentDuration' => $segment->getDuration() ? $playTranslationsHelper->secondsToWords($segment->getDuration()) : null,
+            'programmesContainingSegment' => $programmes,
         ]);
+    }
+
+    /**
+     * Work out what entity should be used for the branding of the Segment by
+     * looking at the Programmes this Segment has appeared on.
+     *
+     * The Segment Owner is the programme that the segment has appeared on
+     * the most. This takes the parents of the ProgrammeItem that the
+     * SegmentEvent was attached to into account.
+     *
+     * If a Segment has appeared only within a single Programme then use that
+     * Programme for Branding. Note that this applies to all levels of a
+     * hierarchy, so a Segment that has appeared within a single Episode, shall
+     * use that episode as its owner, however if a Segment has appeared on
+     * multiple episodes within a single Brand then the Brand shall be used as
+     * the owner.
+     *
+     * This method is public to make it easier to test.
+     *
+     * @param Programme[] $programmeItems
+     * @param CoreEntitiesService $coreEntitiesService
+     * @return CoreEntity|null|Network
+     */
+    public function getSegmentOwner(array $programmeItems, CoreEntitiesService $coreEntitiesService)
+    {
+        // If a segment was not a part of any Programmes then don't set any
+        // explicit branding
+        if (empty($programmeItems)) {
+            return null;
+        }
+
+        // Otherwise we need check for a ancestor that is common to all the
+        // Programmes the Segment was a part of
+
+        $brandingEntity = $this->getCommonProgrammeAncestor($programmeItems);
+        if ($brandingEntity) {
+            // If an ancestor is found, then we need to get it's full representation
+            return $coreEntitiesService->findByPidFull($brandingEntity->getPid());
+        }
+
+        // If there is no common programme, check for a common network
+        return $this->getCommonNetwork($programmeItems);
+    }
+
+    /**
+     * Given a list of Programme Items, find a common ancestor that they all
+     * share.
+     *
+     * @param Programme[] $programmeItems
+     */
+    private function getCommonProgrammeAncestor(array $programmeItems): ?Programme
+    {
+        // The initial list of potentialAncestors is the ancestry of the first
+        // programme
+        $potentialAncestors = $programmeItems[0]->getAncestry();
+
+        // Check each of the other ancestries, and compare them against the
+        // potential ancestors. If an item in the potentialAncestors is not
+        // present in the current item's ancestry then remove it from the list
+        // of potential ancestors.
+        // Once this loop is complete then potential ancestors shall contain
+        // only the items that are in every programme's ancestry
+        foreach ($programmeItems as $programme) {
+            $currentItemAncestryPids = array_map(function ($p) {
+                return (string) $p->getPid();
+            }, $programme->getAncestry());
+
+            $potentialAncestors = array_filter($potentialAncestors, function ($potentialCommonAncestor) use ($currentItemAncestryPids) {
+                $pid = (string) $potentialCommonAncestor->getPid();
+                return in_array($pid, $currentItemAncestryPids, true);
+            });
+
+            // If there are no more possible programmes, then there is nothing
+            // left to filter, so we can exit out of the loop
+            if (empty($potentialAncestors)) {
+                return null;
+            }
+        }
+
+        // There may be a case where an Episode within a Container is the common
+        // ancestor for a SegmentEvent. In this case both the Episode and its
+        // parent (the Container) will be present in the list of potential
+        // ancestors. We want to return the most specific item - the Episode.
+        // Because getAncestry() is ordered by most specifc ancestor first (and
+        // thus TLEO last) and because array_filter preserves array keys, in
+        // cases where there are multiple items in the list of potential
+        // ancestors the most specific potenial ancestor shall the at the start
+        // of the array.
+        return reset($potentialAncestors);
+    }
+
+    /**
+     * Given a list of Programme Items, find a common network that they all
+     * share.
+     *
+     * @param Programme[] $programmes
+     */
+    private function getCommonNetwork(array $programmes): ?Network
+    {
+        // The initial potential network is that of the first programme
+        $potentialNetwork = $programmes[0]->getNetwork();
+        if ($potentialNetwork == null) {
+            return null;
+        }
+
+        // Check each of the other networks. If the potential network does not
+        // match the current item's network then there is no common ancestor
+        foreach ($programmes as $programme) {
+            if ($potentialNetwork != $programme->getNetwork()) {
+                return null;
+            }
+        }
+
+        return $potentialNetwork;
     }
 }
---
+++ tests/Controller/FindByPid/SegmentControllerSegmentOwnerTest.php
@@ -0,0 +1,133 @@
+<?php
+declare(strict_types = 1);
+namespace Tests\App\Controller\FindByPid;
+
+use App\Controller\FindByPid\SegmentController;
+use BBC\ProgrammesPagesService\Domain\Entity\Programme;
+use BBC\ProgrammesPagesService\Domain\Entity\ProgrammeItem;
+use BBC\ProgrammesPagesService\Domain\Entity\Network;
+use BBC\ProgrammesPagesService\Domain\ValueObject\Pid;
+use BBC\ProgrammesPagesService\Service\CoreEntitiesService;
+use PHPUnit\Framework\TestCase;
+
+class SegmentControllerSegmentOwnerTest extends TestCase
+{
+    public function testHasNoContextWithNoItems()
+    {
+        $controller = new SegmentController();
+        $mockService = $this->createMock(CoreEntitiesService::class);
+
+        $items = [];
+        $brandingContext = $controller->getSegmentOwner($items, $mockService);
+
+        $this->assertNull($brandingContext);
+    }
+
+    public function testHasNoContextWithDiverseItems()
+    {
+        $controller = new SegmentController();
+        $mockService = $this->createMock(CoreEntitiesService::class);
+
+        $commonNetwork = $this->createMock(Network::class);
+        $parent = $this->mockProgramme('b0000005', null);
+
+        $items = [
+            $this->mockProgramme('b0000001', null, $commonNetwork),
+            $this->mockProgramme('b0000002', $parent, $commonNetwork),
+            $this->mockProgramme('b0000003', $parent, null),
+        ];
+
+        $brandingContext = $controller->getSegmentOwner($items, $mockService);
+
+        $this->assertNull($brandingContext);
+    }
+
+
+    public function testHasProgrammeContextWithCommonProgramme()
+    {
+        $controller = new SegmentController();
+
+        $mockService = $this->mockServiceThatReturnsItemWithPid('b0000001');
+
+        $commonNetwork = $this->createMock(Network::class);
+        $parent = $this->mockProgramme('b0000005', null, $commonNetwork);
+        $programme = $this->mockProgramme('b0000001', $parent, $commonNetwork);
+
+        $items = [
+            $programme,
+            $programme,
+            $programme,
+        ];
+
+        $brandingContext = $controller->getSegmentOwner($items, $mockService);
+
+        $this->assertEquals('b0000001', $brandingContext->getPid());
+    }
+
+    public function testHasProgrammeContextWithCommonParentProgramme()
+    {
+        $controller = new SegmentController();
+        $mockService = $this->mockServiceThatReturnsItemWithPid('b0000005');
+
+        $commonNetwork = $this->createMock(Network::class);
+        $parent = $this->mockProgramme('b0000005', null, $commonNetwork);
+
+        $items = [
+            $this->mockProgramme('b0000001', $parent, $commonNetwork),
+            $this->mockProgramme('b0000002', $parent, $commonNetwork),
+            $this->mockProgramme('b0000003', $parent, $commonNetwork),
+        ];
+
+        $brandingContext = $controller->getSegmentOwner($items, $mockService);
+
+        $this->assertEquals('b0000005', $brandingContext->getPid());
+    }
+
+
+    public function testHasNetworkContextWithCommonNetwork()
+    {
+        $controller = new SegmentController();
+        $mockService = $this->createMock(CoreEntitiesService::class);
+
+        $commonNetwork = $this->createMock(Network::class);
+        $parent = $this->mockProgramme('b0000005', null);
+
+        $items = [
+            $this->mockProgramme('b0000001', $parent, $commonNetwork),
+            $this->mockProgramme('b0000002', $parent, $commonNetwork),
+            $this->mockProgramme('b0000003', null, $commonNetwork),
+        ];
+
+        $brandingContext = $controller->getSegmentOwner($items, $mockService);
+
+        $this->assertEquals($commonNetwork, $brandingContext);
+    }
+
+    private function mockServiceThatReturnsItemWithPid(string $expectedPid)
+    {
+        $mockProgramme = $this->createConfiguredMock(ProgrammeItem::class, [
+            'getPid' => new Pid($expectedPid),
+        ]);
+
+        $mockService = $this->createMock(CoreEntitiesService::class);
+        $mockService->expects($this->once())->method('findByPidFull')
+            ->with(new Pid($expectedPid))
+            ->willReturn($mockProgramme);
+
+        return $mockService;
+    }
+
+    private function mockProgramme(string $pid, ?Programme $parent = null, ?Network $network = null)
+    {
+        $mock = $this->getMockBuilder(Programme::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['getPid', 'getParent', 'getNetwork'])
+            ->getMock();
+
+        $mock->method('getPid')->willReturn(new Pid($pid));
+        $mock->method('getParent')->willReturn($parent);
+        $mock->method('getNetwork')->willReturn($network);
+
+        return $mock;
+    }
+}
