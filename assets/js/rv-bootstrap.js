7b86da170cf9c8b6b5008c27ffae677b8d1d6871
Michael Gwynne <votemike@users.noreply.github.com>
2018-10-09 10:41:36 +0100

    Trying to recruit (#723)

--- assets/js/rv-bootstrap.js
+++ assets/js/rv-bootstrap.js
@@ -18,6 +18,16 @@ define(['jquery', 'lazyload', 'istats-tracking', 'lazysizes'],function($, Lazylo
                 tracking.trackLinks(context.content);
             }
         });
+
+        var fontFamily = "font-family: Helvetica, Arial, sans-serif",
+            fontStyling = fontFamily + "; font-size: 32px; line-height: 1.5; color: #fff",
+            beebBlock = fontStyling + "; background-color: #000; padding: 4px 14px",
+            dAndEBlock = fontStyling + "; background-color: #c8c8c8; color: #000; padding: 4px 10px",
+            l = fontFamily + "; font-size: 14px; line-height: 1.15rem",
+            c = l + "; text-decoration: underline",
+            blockEnding = "";
+        console.log("\n%cB%c %cB%c %cC%c %cDesign + Engineering%c\n\n%cIf you're interested in how this website was built, visit https://github.com/bbc/programmes-frontend\n" +
+            "If you're looking for a BBC role in Technology, Design and Engineering; visit https://careerssearch.bbc.co.uk/jobs/category/57%c\n%c\n", beebBlock, blockEnding, beebBlock, blockEnding, beebBlock, blockEnding, dAndEBlock, blockEnding, l, c, l);
     });
 
     return {
