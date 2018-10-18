b20494e81b73d8138c8f8336acf8fe38d1d59a5c
TomWinnington <tom.franciswinnington@gmail.com>
2018-10-18 12:40:40 +0100

    built podcast template

--- src/Controller/Podcast/EpisodesDownloadsController.php
+++ src/Controller/Podcast/EpisodesDownloadsController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Podcast;
 
 use App\Controller\BaseController;
 use App\Ds2013\PresenterFactory;
+use BBC\ProgrammesPagesService\Domain\Entity\Programme;
 use BBC\ProgrammesPagesService\Domain\ValueObject\Pid;
 use BBC\ProgrammesPagesService\Service\ProgrammesService;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -16,17 +17,18 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
  */
 class EpisodesDownloadsController extends BaseController
 {
-    public function __invoke(string $pid, ProgrammesService $programmesService, PresenterFactory $presenterFactory)
+    public function __invoke(Programme $programme)
     {
-        $programme = $programmesService->findByPidFull(new Pid($pid));
-        if (!$programme) {
-            throw new NotFoundHttpException(sprintf('Unknown Programme with PID "%s"', $pid));
+        if (!$programme->isTleo()) {
+            return $this->cachedRedirectToRoute('programme_podcast_episodes_download', ['pid'=>$programme->getTleo()->getPid()],301);
         }
 
         $this->setContextAndPreloadBranding($programme);
 
         $this->overridenDescription = 'Podcast downloads for ' . $programme->getTitle();
 
-        return $this->renderWithChrome('podcast/podcast.html.twig');
+        return $this->renderWithChrome('podcast/podcast.html.twig',[
+            'programme'=> $programme,
+        ]);
     }
 }
