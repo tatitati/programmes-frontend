ff43fab26d1475a3becfb42ee086c08f0d631add
TomWinnington <tom.franciswinnington@gmail.com>
2018-10-19 13:46:31 +0100

    work in progress

--- src/Controller/Podcast/EpisodesDownloadsController.php
+++ src/Controller/Podcast/EpisodesDownloadsController.php
@@ -5,8 +5,11 @@ namespace App\Controller\Podcast;
 
 use App\Controller\BaseController;
 use App\Ds2013\PresenterFactory;
+use BBC\ProgrammesPagesService\Domain\Entity\Collection;
+use BBC\ProgrammesPagesService\Domain\Entity\CoreEntity;
 use BBC\ProgrammesPagesService\Domain\Entity\Programme;
 use BBC\ProgrammesPagesService\Domain\ValueObject\Pid;
+use BBC\ProgrammesPagesService\Service\PodcastsService;
 use BBC\ProgrammesPagesService\Service\ProgrammesService;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -17,18 +20,33 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
  */
 class EpisodesDownloadsController extends BaseController
 {
-    public function __invoke(Programme $programme)
+    public function __invoke(CoreEntity $coreEntity, PodcastsService $podcastsService)
     {
-        if (!$programme->isTleo()) {
-            return $this->cachedRedirectToRoute('programme_podcast_episodes_download', ['pid' => $programme->getTleo()->getPid()], 301);
+        if (!$coreEntity instanceof CoreEntity && !$coreEntity instanceof Collection) {
+            throw new NotFoundHttpException(sprintf('Core Entity with PID "%s" is not a programme or collection', $pid));
         }
 
-        $this->setContextAndPreloadBranding($programme);
+        if ((!$coreEntity instanceof Collection) && !$coreEntity->isTleo()) {
+            return $this->cachedRedirectToRoute('programme_podcast_episodes_download', ['pid' => $coreEntity->getTleo()->getPid()], 301);
+        }
 
-        $this->overridenDescription = 'Podcast downloads for ' . $programme->getTitle();
+        $this->setContextAndPreloadBranding($coreEntity);
+
+        $this->overridenDescription = 'Podcast downloads for ' . $coreEntity->getTitle();
+        $podcast = $podcastsService->findByCoreEntity($coreEntity);
+
+        $programme;
+        if($coreEntity instanceof Collection){
+            $programme =$coreEntity->getParent();
+        }else{
+            $programme = $coreEntity;
+        }
+
 
         return $this->renderWithChrome('podcast/podcast.html.twig', [
             'programme' => $programme,
+            'entity' => $coreEntity,
+            'podcast' => $podcast,
         ]);
     }
 }
